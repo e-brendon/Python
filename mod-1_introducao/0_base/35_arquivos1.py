4364f2d1730d046bf867961bd153a10057fd032e
Brendon Esteves <brendonmontteiro@gmail.com>
2025-06-21 20:57:35 -0400

    ajustes nos arquivos

--- mod-1_introducao/0_base/35_arquivos1.py
+++ mod-1_introducao/0_base/35_arquivos1.py
@@ -3,9 +3,15 @@ arquivo = open('./mod-1_introducao/0_base/arquivos/aula63.txt', 'a')
 i = 0
 
 while True:
-    if i > 3:
+    if i > 1:
         break
     
     nome = input ("Digite aqui o nome: ")
     arquivo.write(nome + '\n')
     i += 1
+    
+
+arquivo_opn = open('./mod-1_introducao/0_base/arquivos/aula63.txt', 'r')
+leitura = arquivo_opn.read()
+
+print (leitura)
