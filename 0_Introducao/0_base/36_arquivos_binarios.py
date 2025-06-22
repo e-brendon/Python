b99f43f38d7e097b358bbc947a0001c0f33c636d
Brendon Esteves <brendonmontteiro@gmail.com>
2025-06-22 15:54:23 -0400

    finalizou

--- 0_Introducao/0_base/36_arquivos_binarios.py
+++ 0_Introducao/0_base/36_arquivos_binarios.py
@@ -1,1 +1,22 @@
 import pickle
+
+x = {
+    'nome': 'brendon',
+    'idade': '26',
+    'é': 'isso'
+}
+
+print (type(x))
+
+string = pickle.dumps(x)
+
+print (pickle.loads(string))
+
+y = [1, 2, 3]
+
+arq = open('./0_introducao/0_base/arquivos/aula64.pkl', 'wb')
+pickle.dump(y, arq)
+
+arq = open('./0_introducao/0_base/arquivos/aula64.pkl', 'rb')
+retorno = pickle.load(arq)
+print (retorno)
