c30fa9d5da43dc323742e3f86c28a9aba9a22307
Brendon Esteves <brendonmontteiro@gmail.com>
2025-06-29 16:53:35 -0400

    construtor

---
+++ 0_Introducao/1_POO/2_construtor.py
@@ -0,0 +1,6 @@
+class Pessoas:
+    def __init__(self, nome, idade, cpf):
+        print(f'{nome} | {idade} | {cpf}')
+        
+p1 = Pessoas('Brendon', 26, 46555555590 )
+p2 = Pessoas('Viviane', 26, 44442221113)
